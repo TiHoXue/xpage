ea83b83046178555c23bbfc91597c73a63fdbb56
xueth <xuethlovelicy@gmail.com>
2024-10-10 13:27:18 +0800

    Restart

--- README.md
+++
@@ -1,5 +0,0 @@
-# xpage
-
-This is my personal webpage. Thanks for visiting!
-
-URL: [xuethlovelicy.xyz](https://xuethlovelicy.xyz)
--- app/config/external_links.py
+++
@@ -1,6 +0,0 @@
-# external_links.py
-
-EXTERNAL_LINKS = [
-    {"name": "Cursor", "url": "https://cursor.sh/"},
-    {"name": "霞骛文楷", "url": "https://github.com/lxgw/LxgwWenKai"},
-]
